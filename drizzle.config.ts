9b82a97a2580c062f08df3a9fb7106968b96ee2d
Souvik Kolay <souvik.kolay.1594@gmail.com>
2025-06-18 21:12:34 +0530

    supabase and drizzle commit v2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schemas/*.ts",
   out: "./src/db/migration/drizzle",
   dbCredentials: {
-    url: "postgresql://postgres.blgsvuyzaebdzocqecgd:Souvik@310594!@aws-0-ap-south-1.pooler.supabase.com:6543/postgres",
+    url: process.env.NEXT_PUBLIC_DB_CONNECTIONSTRING!,
   },
 });
